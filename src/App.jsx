2d2cfa2a4555059cba61b864861b8ae7f639e375
min-tcs <17083526+min-tcs@users.noreply.github.com>
2024-09-07 08:48:52 -0500

    generated gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,9 @@ function App() {
       {/* <ParentCounter /> */}
       {/* <Form/> */}
       {/* <FormikForm/> */}
-      {/* <ToDoList/> */}
+      <ToDoList/>
       {/* <UseEffectVariations/> */}
-      {<ReactRouterDOM/>}
+      {/* {<ReactRouterDOM/>} */}
     </>
     
   )
